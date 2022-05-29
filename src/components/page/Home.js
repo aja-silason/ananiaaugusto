51ed71657c8bf16de79cafed42e06a1caa403bf8
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-05-29 19:15:06 +0100

    Segundo commit

--- src/components/page/Home.js
+++ src/components/page/Home.js
@@ -5,6 +5,14 @@ import Links from "../layout/links";
 import LinkButton from "../layout/links";
 import styles from './Home.module.css'
 
+import icongithub from '../assets/iconsgithub.png'
+import iconfacebook from '../assets/iconsfacebook.png'
+import iconinstagram from "../assets/iconsinstagram.png"
+
+import geral from "../assets/geral.png"
+
+import pdf from './assets/anania_augusto_cv.pdf'
+
 export default function Home(){
     return(
         <Conteiner className="conteiner">
@@ -13,20 +21,25 @@ export default function Home(){
 
             </div>
             <div className={styles.conteudo}>
-                <div className={styles.dir}></div>
+                <div className={styles.dir}>
+                asd
+                </div>
                 <div className={styles.esq}>
                     <h1>Anania Jaime Augusto</h1>
                     <p>bfkhwf fbkbgfyufgnuerk gerghkgkug ng jrkgberg er ghuerbgukerg rgreg uerhgu krengnçfluehf ewfwuiiuwb bfkhwf fbkbgfyufgnuerk gerghkgkug ng jrkgberg er ghuerbgukerg rgreg uerhgu krengnçfluehf ewfwuiiuwb bfkhwf fbkbgfyufgnuerk gerghkgkug ng jrkgberg er ghuerbgukerg rgreg uerhgu krengnçfluehf ewfwuiiuwb</p>
+
+                    <a href="./anania_augusto_cv.pdf" target="_black" className={styles.a}>Ver C.V</a>
+
                 </div>
             </div>
             
             <div className={styles.footer}>
                 
-                <a href="https://github.com/aja-silason" target="_black">gitHub</a>
+                <a href="https://github.com/aja-silason" target="_black" className={styles.a}><img src={icongithub}/>github</a>
 
-                <a href="https://www.facebook.com/profile.php?id=100076321422246" target="_black">facebook</a>
+                <a href="https://www.facebook.com/profile.php?id=100076321422246" target="_black" className={styles.a}><img src={iconfacebook} />facebook</a>
 
-                <a href="https://www.instagram.com/ananias_augusto/" target="_black">instagram</a>
+                <a href="https://www.instagram.com/ananias_augusto/" target="_black" className={styles.a}><img src={iconinstagram}/>instagram</a>
 
             </div>
         </Conteiner>
